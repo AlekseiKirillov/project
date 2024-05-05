04acd40bea0e57936054d69660c424341a4314ca
aleksei kirillov <alexkirillovmgn@gmail.com>
2024-05-05 16:11:10 +0400

    Добавил и настроил phpunit

--- rector.php
+++ rector.php
@@ -14,7 +14,7 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/config',
         __DIR__ . '/public',
         __DIR__ . '/src',
-        // __DIR__ . '/tests',
+        __DIR__ . '/tests',
     ]);
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_83,
