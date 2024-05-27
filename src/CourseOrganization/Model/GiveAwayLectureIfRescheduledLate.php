d46fab63f0f0a30e850fdf7d0132ee72e4c3742e
aleksei kirillov <alexkirillovmgn@gmail.com>
2024-05-27 15:20:19 +0400

    Добавлен сага GiveAwayLectureIfRescheduledLate

---
+++ src/CourseOrganization/Model/GiveAwayLectureIfRescheduledLate.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CourseOrganization\Model;
+
+use App\Infrastructure\MessageBus\MessageBus;
+use App\Infrastructure\Uuid\Uuid;
+
+final class GiveAwayLectureIfRescheduledLate
+{
+    private function __construct(
+        private readonly Uuid $lectureId,
+        private readonly Uuid $groupId,
+        public \DateTimeImmutable $scheduledStartTime,
+    ) {}
+
+    public static function createFromLectureScheduled(LectureScheduled $lectureScheduled): self
+    {
+        return new self(
+            lectureId: $lectureScheduled->lectureId,
+            groupId: $lectureScheduled->groupId,
+            scheduledStartTime: $lectureScheduled->scheduledStartTime,
+        );
+    }
+
+    public function onLectureRescheduled(LectureRescheduled $event, MessageBus $messageBus): void
+    {
+        if ($event->at > $this->scheduledStartTime->modify('-1 day')) {
+            $messageBus->dispatch(new GiveAwayLecture($this->groupId));
+        }
+
+        $this->scheduledStartTime = $event->newScheduledStartTime;
+    }
+}
